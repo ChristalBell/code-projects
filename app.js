f6f854d1fd5a6a38d0affab06faac23851f48114
Christal Bell <christalvictoriabell@gmail.com>
2023-05-10 22:15:10 -0700

    starting js still not function

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const onClick = () => {
+    const validEmail = document.getElementById("email"); 
+}
 
+const validEmail = (email) => {
+    if (email.value === ""){
+        return errorMessage.innerHTML = "please provide a valid email. " ;
+    } else {
+        return false ; 
+    }
+       
+}
