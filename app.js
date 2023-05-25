a9bd53c198d54a6f8a16d3915e1f9361b9a1c16d
Christal Bell <christalvictoriabell@gmail.com>
2023-05-24 19:14:24 -0700

    js finally working :)

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const email = document.getElementById ("email")
-const onSubmit = document.getElementById("onSubmit")
-
-const validateEmail = (email) => {
-    if (email === null ) {
-        return document.getElementById("errorMessage").innerHTML += "Please enter a valid email.";
-    } 
+onSubmit= () => {
+    const email=document.getElementById("email");
+    if (email.value === "") {
+        email.classList.add("error");
+    }
 }
