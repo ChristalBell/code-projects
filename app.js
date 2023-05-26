84682489c526848478ab339bf478b2adbc89c53d
Christal Bell <christalvictoriabell@gmail.com>
2023-05-25 20:51:55 -0700

    js console log work, inner html no

--- app.js
+++ app.js
@@ -2,5 +2,18 @@ onSubmit= () => {
     const email=document.getElementById("email");
     if (email.value === "") {
         email.classList.add("error");
+        console.log ("Please enter a valid email.");
     }
 }
+validate = ()=> {
+    
+    const errorMessage= document.getElementById("error-message");
+    if (onSubmit= true){
+        errorMessage.innerHTML += "Please enter a valid email."
+    }
+}
+
+
+
+
+        
